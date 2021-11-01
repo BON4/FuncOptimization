778261be407b0c8bcf98d8218306f32f207b8ef7
BON4 <48278952+BON4@users.noreply.github.com>
2021-11-01 11:34:44 +0300

    Fixed ploting for multiple constrains

--- constarint_optimization.py
+++ constarint_optimization.py
@@ -5,54 +5,43 @@ import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 from scipy.optimize import LinearConstraint
 from matplotlib import cm
-from tabulate import tabulate
+import tabulate
+import math
 
-START_POINT = [1, 0]
+START_POINT = [2,2]
 
+# fun - main muction
+def fun(x: np.ndarray) -> np.float64:
+    return ((x[0]-2)**2) + (x[1]-1)**2
 
-def fun(x: np.ndarray) -> np.float:
-    # return x[0]**4+x[1]**4
-    return (np.exp(x[0]) + np.exp(x[1]))
+# hfunc - represents an "= 0" fun
+def hfun(x: np.ndarray) -> np.float64:
+    return x[0]-2*x[1]+1
 
-
-def hfun(x: np.ndarray) -> np.float:
-    return (x[0]**2+x[1]**2-9)
-
-
-def gfun1(x: np.ndarray) -> np.float:
-    # return (6*x[0]-x[1]**2-6)
-    return (x[0]+x[1]-1)
-
-
-def gfun2(x: np.ndarray) -> np.float:
-    return x[0]
-
-
-def gfun3(x: np.ndarray) -> np.float:
-    return x[1]
+# gfunc - represents an ">= 0" fun
+def gfun(x: np.ndarray) -> np.float64:
+    return -0.25 * x[0]**2 - x[1]**2+1
 
 
 def gplusfun(g, x, u, c):
     return min(g(x), np.divide(u, c))
 
 
-def modified_lagrange_method(fun, points, epsx, g_constarins, h_constrains, u=[0], a=[0], c=0.1, beta=2, counter=0):
+def modified_lagrange_method(fun, points, epsx, g_constarins, h_constrains, u=[0], a=[0], c=0.1, beta=8, counter=0,func_counter = 0, _callback=None):
     """Minimize a function with given constrains
-
     Arguments:
         points {[float]} -- [array of calculated points]
         epsx {[float]} -- [epsilon]
         g_constarins {[Callable]} -- [array of inequality constrains]
         h_constrains {[Callable]} -- [array of equality constrains]
-
     Keyword Arguments:
         u {list} -- [Langrange factor for inequality] (default: {[0]})
         a {list} -- [Langrange factor for equality] (default: {[0]})
         c {float} -- [penalty factor] (default: {0.1})
         beta {int} -- [growth rate of penalty factor must be in range [2;26]] (default: {2})
         counter {int} -- [counter] (default: {0})
+        callback - function that takes dict x, witch contains all intermediate values such as x, u, a, c, f(x), L(x,u,a,c)
     """
-
     def lagrange(x):
         if(len(g_constarins) != 0 and len(g_constarins) != 0):
             array_of_constrains_g = np.array(
@@ -70,10 +59,15 @@ def modified_lagrange_method(fun, points, epsx, g_constarins, h_constrains, u=[0
             return fun(x) - sum([u_i * g for u_i, g in zip(u, array_of_constrains_g)]) + 0.5*sum(c * array_of_constrains_g**2)
         else:
             return fun(x)
+    
+    if _callback is not None:
+        _callback({"x": points[-1], "u": u, "a": a, "c": c, "f": fun(points[-1]), "L": lagrange(points[-1]), "iter": counter, "fiter": func_counter})
 
-    next_val = sopt.minimize(
-        lagrange, x0=points[-1], method='BFGS').x
-
+    res = sopt.minimize(
+        lagrange, x0=points[-1], method='BFGS')
+    next_val = res.x
+    func_counter = func_counter+res.nfev
+    counter = counter+res.nit
     points.append(next_val)
 
     u = np.array([max(0, (u_i - c*g_constrain(next_val)))
@@ -89,39 +83,89 @@ def modified_lagrange_method(fun, points, epsx, g_constarins, h_constrains, u=[0
     if(abs(next_val - points[-2])[0] < epsx and abs(next_val - points[-2])[1] < epsx):
         return points
     else:
-        return modified_lagrange_method(fun, points, epsx, g_constarins, h_constrains, u, a, c, beta, counter)
+        return modified_lagrange_method(fun, points, epsx, g_constarins, h_constrains, u, a, c, beta, counter, func_counter,_callback)
 
+
+def filter_zLim(X,Y,Z, zlim):
+    for i in range(0, len(Z)):
+        for j in range(0, len(Z)):
+            if Z[i][j] > zlim[1] or Z[i][j] < zlim[0]:
+                Z[i][j] = 4
+
+    return X, Y, Z
+
+def printDecorator(f, res):
+    def wrapper(x):
+        res.append(x)
+        ret_val = f(x)
+        return ret_val
+    return wrapper
+
+def trunc(number, digits) -> float:
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
 
 def plotting():
+    xs = []
+    results_list = []
+    f = lambda x: xs.append(x["x"])
+    callback = printDecorator(f, results_list)
 
-    X, Y = np.mgrid[-2: 3: 20j, -4.5: 3: 20j]
+    #Adjust plotting scale here where x in [a1,b1] and y in [a2,b2] [a1: b1: 20j, a2: b2: 20j]
+    X, Y = np.mgrid[2.4:0:20j, 2.5:-1.5:20j]
     Z = fun(np.array([X, Y]))
 
     fig = plt.figure()
 
-    ax = fig.add_subplot(1, 1, 1, projection='3d')
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    ax.set_xlim((0, 3))
+    ax.set_ylim((-1.5, 3))
+    ax.set_zlim((0, 4))
 
     ax.plot_wireframe(X, Y, Z)
 
-    ax.contour(X, Y, fun(
-        np.array([X, Y]))-gfun1(np.array([X, Y])), levels=[14], colors='green')
+    ax.contour(X, Y, gfun(np.array([X, Y])), levels=[0], colors='blue')
+    ax.contour(X, Y, hfun(np.array([X, Y])), levels=[0], colors='lime')
 
-    X, Z = np.mgrid[-2: 3: 20j, 0: 20: 20j]
-    Y = (lambda x: np.sqrt(
-        9-x[0]**2))(np.array([X, Y]))
-
-    ax.plot_wireframe(X, -Y, Z)
-
-    g_constarins = np.array([gfun1, gfun2, gfun3])
+    #Put list of constrains here, for my case its one constrain g(x) and one h(x)
+    g_constarins = np.array([gfun])
     h_constrains = np.array([hfun])
 
-    vals = modified_lagrange_method(fun, list([START_POINT, ]), 0.001,
-                                    g_constarins, h_constrains)
+    vals = modified_lagrange_method(fun, list([START_POINT, ]), 1e-6,
+                                    g_constarins, h_constrains, _callback=callback)
 
+    #Print Results Table
+    header = results_list[0].keys()
+    rows =  [x.values() for x in results_list[0:11] + [results_list[-1]]]
+    print(tabulate.tabulate(rows, header, tablefmt='grid'))
+
+    
     ax.plot(np.array(vals).T[0], np.array(vals).T[1], np.array(
         list(map(fun, np.array(vals)))).T, "x-", color='red')
 
-    plt.show()
+    ax1 = fig.add_subplot(1, 2, 2)
 
+    #Adjust plotting scale here where x in [a1,b1] and y in [a2,b2] [a1: b1: 20j, a2: b2: 20j]
+    X, Y = np.mgrid[3: 0: 20j, 2.3: -1.5: 20j]
+    Z = fun(np.array([X, Y]))
+
+    ax1.contour(X, Y, Z, levels=40)
+
+    t = 0
+    for x in zip(np.array(vals).T[0], np.array(vals).T[1]):
+        if abs(fun(x) - t) > 1e-2:
+            ax1.annotate(trunc(fun(x),3), (x[0], x[1]))
+        t = fun(x)
+
+    ax1.plot(np.array(vals).T[0], np.array(vals).T[1], "x-", color='red')
+
+    for idx, g_constr in enumerate(g_constarins):
+        ax1.clabel(ax1.contour(X, Y, g_constr(np.array([X, Y])), levels=[0], colors='blue'), fmt=f"g{idx}(x)", fontsize=10)
+
+    for idx, h_constr in enumerate(h_constrains):
+        ax1.clabel(ax1.contour(X, Y, h_constr(np.array([X, Y])), levels=[0], colors='lime'), fmt=f"h{idx}(x)", fontsize=10)
+
+
+    plt.show()
 
 plotting()
